647b7436b8f2aa96d8ace59e94dacbc1dd455c09
Vishal Sharma <vsh1819@gmail.com>
2018-08-13 05:35:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+const userName = ''
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
+
+//Issue and Repo objects and templates
+
+function Issue(attributes){
+  this.title = attributes.title;
+  this.body = attributes.body;
+  this.url = attributes.url;
+}
+
+function Repo(attributes){
+  this.url = attributes.url;
+}
+
+Issue.prototype.template = function(){
+   var template = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
+   return template;
+};
+
+Repo.prototype.template = function(){
+  var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+  return template;
+};
+
+
+
+
 function getIssues() {
 }
 
